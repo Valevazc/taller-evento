a6e4981b3f6afae03134ab4815170ab823c8892f
Valevazc <valentinavazquezcarro@gmail.com>
2024-08-13 12:46:57 -0300

    Solucioné el problema del evento

--- eventos.js
+++ eventos.js
@@ -1,8 +1,14 @@
-document.addEventListener("DOMContentLoaded", function(){
-
+document.addEventListener("DOMContentLoaded", function() {
+   
     var div = document.querySelector("div");
+    var boton = document.getElementById("saludar");
+
     div.addEventListener("click", function() {
         alert("Hola! Soy el div");
     });
-
 });
+
+    function mostrarAlertaBoton(event) {
+        alert("Hola!");
+        event.stopPropagation();
+ }
